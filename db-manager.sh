663f9b4c4d3c3dd138855f114cd3ccb100cdca69
Nikita <znikidi@gmail.com>
2025-10-29 13:09:01 +0500

    made check funs for crate case

--- db-manager.sh
+++ db-manager.sh
@@ -1,15 +1,40 @@
 #!/bin/bash
 
-# help function
-create_help() {
-    echo "help"
-}
-
-start_help() {
-    #test
-}
+dbs_param = ("postgres" "mysql")
 
 #usage function
 usage() {
     ec
 }
+
+check_db_param()
+{
+    for db in dbs_param; do
+        if [[ "$db" == $1]]; then
+        return 0
+        fi
+    done
+    return 1
+}
+
+
+case "$1" in:
+    "create")
+
+    if test $2 || $3 || $4; then
+    echo "One of the parameters is missing"
+    exit 1
+    fi
+
+    if [[ check_db_param($2) == 1 ]]; then
+    echo "Can't find this type of db. Correct types: ${dbs_param[*]}"
+    exit 1
+
+
+    
+
+
+
+
+
+    
